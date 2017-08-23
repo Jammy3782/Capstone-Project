504da230a0f82b42ae0c9918dc5e2de85410ad4f
Jammy3782 <jammy3782@gmail.com>
2017-08-22 20:20:27 -0700

    restarted repo

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name farmersmarketApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the farmersmarketApp
+ */
+angular.module('farmersmarketApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
